6220ac53e2ce4c5ea7d672581f43d07b8dc1f2c0
jagdeepsinghvirdi <147831048+jagdeepsinghvirdi@users.noreply.github.com>
2025-06-13 11:20:18 +0530

    Update hooks.py

--- permission_cloner/hooks.py
+++ permission_cloner/hooks.py
@@ -5,14 +5,3 @@ app_publisher = "Your Name"
 app_description = "Tool to copy role permissions from one role to another"
 app_email = "your@email.com"
 app_license = "MIT"
-
-<<<<<<< HEAD
-# No front-end assets to build
-=======
->>>>>>> 71a6c3d52c815b9e3df0f2906cc22c521058527b
-
-
-# Commented out to skip frontend build issues
-# doctype_js = {
-#     "Permission Cloner": "public/js/permission_cloner.js"
-# }
